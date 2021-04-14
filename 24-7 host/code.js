307a3d177ff5a498fa97c4d71618ed466b955d1d
ara46 <78352220+ara46@users.noreply.github.com>
2021-04-15 01:59:54 +0300

    Update code.js

--- 24-7 host/code.js
+++ 24-7 host/code.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch')
 
-setInterval(async () => {
+setInterval(async (https://glitch.com/edit/#!/gelatinous-level-discovery) => {
   await fetch('Your glitch project live page URL here').then(console.log('Pinged!'))
 }, 240000)
